b6784ac645944f3822c8cfc962e654e764603cf6
Vinh Tran <vinh.tran02@pyramid-consulting.com>
2018-04-16 19:32:04 +0700

    get video qualities list

--- app/src/main/java/smartapps/vlab/testtheoplayersdk/MainFragment.java
+++ app/src/main/java/smartapps/vlab/testtheoplayersdk/MainFragment.java
@@ -9,7 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.theoplayer.android.api.THEOplayerView;
+import com.theoplayer.android.api.event.EventListener;
+import com.theoplayer.android.api.event.player.EndedEvent;
+import com.theoplayer.android.api.event.player.LoadedDataEvent;
+import com.theoplayer.android.api.event.player.PlayEvent;
+import com.theoplayer.android.api.event.player.PlayerEventTypes;
+import com.theoplayer.android.api.player.track.mediatrack.MediaTrack;
+import com.theoplayer.android.api.player.track.mediatrack.quality.VideoQuality;
 import com.theoplayer.android.api.source.SourceDescription;
+
+import java.util.Iterator;
 
 
 /**
@@ -38,12 +47,21 @@ public class MainFragment extends Fragment {
 
         mTHEOplayerView = view.findViewById(R.id.theoplayer_view);
 
-        SourceDescription mSourceDescription = SourceDescription.Builder.sourceDescription("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
+        SourceDescription mSourceDescription = SourceDescription.Builder
+                .sourceDescription("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
                 .poster("http://cdn.theoplayer.com/video/big_buck_bunny/poster.jpg")
                 .build();
 
         mTHEOplayerView.getPlayer().setSource(mSourceDescription);
 
+        mTHEOplayerView.getPlayer().addEventListener(PlayerEventTypes.LOADEDDATA, mLoadVideoEventListener);
+        mTHEOplayerView.getPlayer().addEventListener(PlayerEventTypes.ENDED, mEndedEventListener);
+
+        handleEvent(view);
+    }
+
+
+    private void handleEvent(View view) {
         view.findViewById(R.id.btn_open_share_video).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,8 +93,47 @@ public class MainFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
 
-        //mTHEOplayerView.getPlayer().removeEventListener(PlayerEventTypes.PLAY, mEventListener);
+        mTHEOplayerView.getPlayer().removeEventListener(PlayerEventTypes.PLAY, mPlayEventListener);
+        mTHEOplayerView.getPlayer().removeEventListener(PlayerEventTypes.LOADEDDATA, mLoadVideoEventListener);
+        mTHEOplayerView.getPlayer().removeEventListener(PlayerEventTypes.ENDED, mEndedEventListener);
+
         mTHEOplayerView.onDestroy();
     }
+
+    private EventListener<PlayEvent> mPlayEventListener = new EventListener<PlayEvent>() {
+        @Override
+        public void handleEvent(PlayEvent playEvent) {
+
+        }
+    };
+
+    private EventListener<LoadedDataEvent> mLoadVideoEventListener = new EventListener<LoadedDataEvent>() {
+        @Override
+        public void handleEvent(LoadedDataEvent loadedDataEvent) {
+            System.out.println(">>> loadedDataEvent");
+
+            String qualityList = "";
+
+            Iterator<MediaTrack<VideoQuality>> iterator = mTHEOplayerView.getPlayer().getVideoTracks().iterator();
+            while (iterator.hasNext()){
+                MediaTrack<VideoQuality> mediaTrack = iterator.next();
+
+                Iterator<VideoQuality> videoQualityIterator = mediaTrack.getQualities().iterator();
+                while (videoQualityIterator.hasNext()){
+                    VideoQuality videoQuality = videoQualityIterator.next();
+                    qualityList += videoQuality.getWidth() +  "," ;
+                }
+            }
+
+            System.out.println(">>> qualities : " + qualityList);
+        }
+    };
+
+    private EventListener<EndedEvent> mEndedEventListener = new EventListener<EndedEvent>() {
+        @Override
+        public void handleEvent(EndedEvent endedEvent) {
+            System.out.println(">>> EndedEvent");
+        }
+    };
 
 }
